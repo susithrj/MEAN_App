fab703d13160940ce19a91007fcb41f31656bb3e
susithrj <susithlion@gmail.com>
2020-09-07 00:26:18 +0530

    connect mongodb.

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
 const path = require('path');
 const app = express();
+
 const port = process.env.PORT || 3000;
+
+const  config = require('./config/database')
 const user = require('./routes/users')
 
+const connection = mongoose.connect(config.database);
+if(connection){
+    console.log("database connected")
+}
 app.use(express.static(path.join(__dirname,"public")));
 app.use('/users',user);
 
