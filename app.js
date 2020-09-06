b48544a18648a3c0974f9338823dbae1d7fd6fbd
susithrj <susithlion@gmail.com>
2020-09-07 00:37:06 +0530

    bodyparser included.

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
 const port = process.env.PORT || 3000;
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 const  config = require('./config/database')
 const user = require('./routes/users')
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const router = express.Router();
+const  User = require('../models/user')
 
 router.post("/register",function (req,res) {
-console.log(req.body)
+
+   const  newUser = new User({
+username:req.body.username,
+       name:req.body.name,
+       email:req.body.email,
+    password:req.body.password
+   })
+
 })
 
 router.get("",function (req,res) {
