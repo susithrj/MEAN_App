128e1e9ec94cb14648197afa1f4e1cdcace2f227
susithrj <susithlion@gmail.com>
2020-09-11 21:39:39 +0530

    auth token completed.

--- config/passport.js
+++ config/passport.js
@@ -1,28 +1,24 @@
 const JwtStrategy = require('passport-jwt').Strategy,
     ExtractJwt = require('passport-jwt').ExtractJwt;
-
 const config = require('./database');
-const  User = require('../models/user')
+const User = require('../models/user');
 
-const opts = {}
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); //fromAuthHeader
-opts.secretOrKey = config.secret;
-opts.issuer = 'accounts.examplesoft.com';
-opts.audience = 'yoursite.net';
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
+opts.secretOrKey = config.secret ;
 
 module.exports = function (passport) {
 
-
-    passport.use(new JwtStrategy(opts, function (jwt_payload, done) {
-        User.findUserbyId({_id: jwt_payload._doc._id}, function (err, user) {
+    passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
+        User.findUserbyId({_id: jwt_payload._doc._id}, function(err, user) {
             if (err) {
                 return done(err, false);
             }
             if (user) {
-                return done(null, user);
+                done(null, user);
             } else {
-                return done(null, false);
-                // or you could create a new account
+                done(null, false);
+
             }
         });
     }));
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/user')
+const User = require('../models/user');
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
-const passport = require('passport')
+const passport = require('passport');
 
 router.post("/register", function (req, res) {
 
@@ -18,11 +18,11 @@ router.post("/register", function (req, res) {
             res.json({state: false, msg: "data not inserted"});
         }
         if (user) {
-            res.json({state: true, msg: "data inserted"})
+            res.json({state: true, msg: "data  inserted"});
         }
-    })
 
-})
+    });
+});
 
 router.post("/login", function (req, res) {
     const email = req.body.email;
@@ -32,19 +32,21 @@ router.post("/login", function (req, res) {
         if (err) throw err;
 
         if (!user) {
-            res.json({state: false, msg: "user not found"})
+            res.json({state: false, msg: "No user found"});
+            return false;
+
         }
 
         User.passwordCheck(password, user.password, function (err, match) {
-            if (err) throw err;
+            if (err) throw  err;
 
             if (match) {
-                console.log("email,pass,combination worked");
-                const token = jwt.sign({user}, config.secret, {expiresIn: 86400});
+
+                const token = jwt.sign(user, config.secret, {expiresIn: 86400 * 3});
                 res.json(
                     {
                         state: true,
-                        token: "JWT" + token,
+                        token: "JWT " + token,
                         user: {
                             id: user._id,
                             name: user.name,
@@ -53,20 +55,22 @@ router.post("/login", function (req, res) {
                         }
                     }
                 )
+            } else {
+                res.json({state: false, msg: "password does not match"});
             }
-        })
 
-    })
-})
-router.post('/profile', passport.authenticate('jwt', { session: false }),
-    function(req, res) {
-        res.json({user:req.user});
+        });
+
+    });
+
+
+});
+
+
+router.post('/profile', passport.authenticate('jwt', {session: false}), function (req, res) {
+        res.json({user: req.user});
     }
 );
-
-
-
-
 
 
 module.exports = router;
